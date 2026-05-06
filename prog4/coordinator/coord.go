348b5a428fc00b1588b3996b4bd43b8127c28c72
LambNNot <nsphan1207@gmail.com>
2026-05-06 14:06:48 -0700

    feat: logfile done

--- prog4/coordinator/coord.go
+++ prog4/coordinator/coord.go
@@ -21,6 +21,18 @@ const TASK_TIMEOUT = 500 * time.Millisecond
 const HEARTBEAT_INTERVAL = 10 * time.Second
 const START_DELAY = 0 * time.Millisecond
 
+type logType int
+
+const (
+	HeartbeatFail logType = iota
+	MapAssign
+	MapReassign
+	MapResult
+	ReduceAssign
+	Replicate
+	Search
+)
+
 type phase int
 
 const (
@@ -151,43 +163,50 @@ func handleFailedWorker(coord *Coordinator, failedAddr string) {
 	fmt.Println("Cleaning up failed worker", failedAddr)
 	delete(coord.workers, failedAddr)
 
-	// Remove failed worker from holders
-	holders := coord.mapReplicas[failedAddr]
-	aliveHolders := make([]string, 0)
-	for _, h := range holders {
-		if h != failedAddr {
-			if _, ok := coord.workers[h]; ok {
-				aliveHolders = append(aliveHolders, h)
+	for owner, holders := range coord.mapReplicas {
+		// Check if holders need to be repaired
+		if !slices.Contains(holders, failedAddr) {
+			continue
+		}
+
+		// Remove failed worker from this owner's holders
+		aliveHolders := make([]string, 0)
+		for _, h := range holders {
+			if h != failedAddr {
+				if _, ok := coord.workers[h]; ok {
+					aliveHolders = append(aliveHolders, h)
+				}
 			}
 		}
-	}
-	coord.mapReplicas[failedAddr] = aliveHolders
 
-	// Re-replicate data
-	srcData, err := getIntermediateData(failedAddr, coord.workers[aliveHolders[0]])
-	if err != nil {
-		fmt.Println("failed to fetch replica data:", err)
-		return
-	}
-
-	for addr, client := range coord.workers {
-		if slices.Contains(coord.mapReplicas[failedAddr], addr) {
+		// Add new replica to hold
+		coord.mapReplicas[owner] = aliveHolders
+		source := aliveHolders[0]
+		srcData, err := getIntermediateData(owner, coord.workers[source])
+		if err != nil {
+			fmt.Println("failed to fetch replica data for", owner, "from", source, ":", err)
 			continue
 		}
+		for addr, client := range coord.workers {
+			if slices.Contains(coord.mapReplicas[owner], addr) {
+				continue
+			}
 
-		args := &common.AcceptReplicaArgs{
-			WorkerAddr: failedAddr,
-			MapOutput:  srcData,
+			args := &common.AcceptReplicaArgs{
+				WorkerAddr: owner,
+				MapOutput:  srcData,
+			}
+			reply := &common.AcceptReplicaReply{}
+
+			if err := client.Call("Worker.AcceptReplica", args, reply); err != nil {
+				fmt.Println("failed to replicate", owner, "data to", addr, ":", err)
+				continue
+			}
+
+			coord.mapReplicas[owner] = append(coord.mapReplicas[owner], addr)
+			go log(Replicate, failedAddr, addr)
+			break
 		}
-		reply := &common.AcceptReplicaReply{}
-
-		if err := client.Call("Worker.AcceptReplica", args, reply); err != nil {
-			fmt.Println("failed to replicate to", addr, err)
-			continue
-		}
-
-		coord.mapReplicas[failedAddr] = append(coord.mapReplicas[failedAddr], addr)
-		break
 	}
 }
 
@@ -201,6 +220,7 @@ func sendHeartbeats(coord *Coordinator) error {
 		err := client.Call("Worker.RecvHeartbeat", args, reply)
 		if err != nil {
 			fmt.Println("Heartbeat on", addr, ":", err)
+			go log(HeartbeatFail, addr, nil)
 			go handleFailedWorker(coord, addr)
 			continue
 		}
@@ -267,7 +287,7 @@ func getReduceTask(coord *Coordinator) *common.Task {
 	return nil
 }
 
-func getMapTask(coord *Coordinator) *common.Task {
+func getMapTask(coord *Coordinator, workerAddr string) *common.Task {
 
 	// Check for any stray map tasks
 	for i := range coord.mapTasks {
@@ -275,6 +295,7 @@ func getMapTask(coord *Coordinator) *common.Task {
 		if task.Status != common.Completed && time.Since(task.StartTime) > TASK_TIMEOUT {
 			task.Status = common.InProgress
 			task.StartTime = time.Now()
+			go log(MapReassign, task.Id, workerAddr)
 			return task
 		}
 	}
@@ -298,6 +319,7 @@ func getMapTask(coord *Coordinator) *common.Task {
 		}
 		coord.frontier.toVisit = coord.frontier.toVisit[frontierCutoff:]
 		coord.mapTasks = append(coord.mapTasks, newTask)
+		go log(MapAssign, newTask.Id, workerAddr)
 		return &coord.mapTasks[len(coord.mapTasks)-1]
 	}
 
@@ -318,7 +340,7 @@ func (coord *Coordinator) RequestTask(args *common.RequestTaskArgs, reply *commo
 	switch coord.phase {
 	case mapPhase:
 		// fmt.Println("Map task requested")
-		newTask = *getMapTask(coord)
+		newTask = *getMapTask(coord, args.WorkerAddr)
 
 	case reducePhase:
 		// fmt.Println("Reduce task requested")
@@ -327,6 +349,7 @@ func (coord *Coordinator) RequestTask(args *common.RequestTaskArgs, reply *commo
 			newTask = *waitTask()
 		} else {
 			newTask = *task
+			go log(ReduceAssign, newTask.Id, args.WorkerAddr)
 		}
 
 	case completed:
@@ -410,6 +433,9 @@ func (coord *Coordinator) ReportTask(args *common.ReportTaskArgs, reply *common.
 
 		// Mark it as an original data owner
 		coord.dataOwners[args.WorkerAddr] = true
+		go log(MapResult, args.TaskID, nil)
+		go log(Replicate, args.WorkerAddr, replica1)
+		go log(Replicate, args.WorkerAddr, replica2)
 
 	case common.Reduce:
 		if args.TaskID < 0 || args.TaskID >= len(coord.reduceTasks) {
@@ -572,4 +598,38 @@ func (coord *Coordinator) GetNewReplica(
 	}
 
 	return fmt.Errorf("no available worker for new replica of %s", args.Original)
+}
+
+func log(event logType, arg1 any, arg2 any) {
+	message := ""
+	switch event {
+	case HeartbeatFail:
+		failedAddr := arg1
+		message = fmt.Sprintf("HEARTBEAT FAILED FROM WORKER %s\n", failedAddr)
+	case MapAssign:
+		mapTaskID, workerAddr := arg1, arg2
+		message = fmt.Sprintf("ASSIGN MAP TASK %d TO WORKER %s\n", mapTaskID, workerAddr)
+	case MapReassign:
+		mapTaskID, workerAddr := arg1, arg2
+		message = fmt.Sprintf("REASSIGN MAP TASK %d TO WORKER %s\n", mapTaskID, workerAddr)
+	case MapResult:
+		mapTaskID := arg1
+		message = fmt.Sprintf("GET MAP TASK %d RESULT\n", mapTaskID)
+	case ReduceAssign:
+		reduceTaskID, workerAddr := arg1, arg2
+		message = fmt.Sprintf("ASSIGN REDUCE TASK %d TO WORKER %s\n", reduceTaskID, workerAddr)
+	case Replicate:
+		srcAddr, destAddr := arg1, arg2
+		message = fmt.Sprintf("REPLICATE %s TO WORKER %s\n", srcAddr, destAddr)
+	case Search:
+		keyword, workerAddr := arg1, arg2
+		message = fmt.Sprintf("ASSIGN SEARCH %s TO WORKER %s\n", keyword, workerAddr)
+	}
+	f, err := os.OpenFile("/app/logs/logfile.txt", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+	if err != nil {
+		panic(err)
+	}
+	_, err = f.WriteString(message)
+
+	defer f.Close()
 }
